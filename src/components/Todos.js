edb443d785300b397e79435bb4a0785e3ccbeace
AnjaP98 <97261181+AnjaP98@users.noreply.github.com>
2022-02-07 14:29:27 +0100

    dodat broj preostalih taskova

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -21,6 +21,28 @@ function Todos(){
       isCompleted: false
     }
   ]);
+  
+  function toggleTodoCompleted(id) {
+    const updatedTodos = todos.map(todo => {
+      
+      if (id === todo.id) {
+        
+        return {...todo, isCompleted: !todo.complete}
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  }
+  const todoList = todos.map(todo => (
+    <Todo
+        id={todo.id}
+        name={todo.name}
+        completed={todo.isCompleted}
+        key={todo.id}
+        toggleTodoCompleted={toggleTodoCompleted}
+      />
+    )
+  );
 
   
 
@@ -40,16 +62,18 @@ function Todos(){
     newTodos.splice(index, 1);
     setTodos(newTodos);
   };
-
+const preostalo = `Ukupno preostalih taskova: ${todoList.length}`;
   return (
     <div className="app">
+    
+    <h3 id="list-heading">{preostalo}</h3> // koliko je preostalo taskova
       <div className="todo-list">
         {todos.map((todo, index) => (
           <Todo
             key={index}
             index={index}
             todo={todo}
-            completeTodo={completeTodo}
+           
             removeTodo={removeTodo}
           />
         ))}
